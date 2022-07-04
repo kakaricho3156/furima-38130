f9ed1674733cb69e0ffa32ba16f38854f4e54e14
kakaricho3156 <kakaricho1216@gmail.com>
2022-07-04 21:33:35 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         validates :nickname, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+            validates :nickname, presence: true
          validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
          validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
