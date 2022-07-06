c9ebc593edc179fe7c8befd21f80bc4363e46fdb
kakaricho3156 <kakaricho1216@gmail.com>
2022-07-06 14:51:51 +0900

    validatesの設定から

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,22 @@ class ItemsController < ApplicationController
   def index
   end
   
+  def new
+    @item=Item.new
+  end
+
+  def create
+   Item.create(item_params)
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name,:item_detail,:item_category_id,:item_condition_id,:delivery_charge_id,:shipping_area_id,:days_to_ship_id,:price)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates :content, presence: true
+  validates :content, presence: true
+  validates :content, presence: true
+  validates :content, presence: true
+  validates :content, presence: true
+  validates :content, presence: true
 end
