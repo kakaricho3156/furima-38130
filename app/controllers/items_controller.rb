2bf2957ad8eca47829aa106a08f81b12ffd9bdff
kakaricho3156 <kakaricho1216@gmail.com>
2022-07-07 22:53:16 +0900

    js実装から

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except: [:show,:index]
+before_action :set_item, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
   
   def new
@@ -8,6 +12,11 @@ class ItemsController < ApplicationController
 
   def create
    Item.create(item_params)
+   if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
   end
 
   def edit
@@ -18,6 +27,10 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_name,:item_detail,:item_category_id,:item_condition_id,:delivery_charge_id,:shipping_area_id,:days_to_ship_id,:price)
+    params.require(:item).permit(:item_name,:item_detail,:item_category_id,:item_condition_id,:delivery_charge_id,:shipping_area_id,:days_to_ship_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,23 @@
 class Item < ApplicationRecord
-  validates :content, presence: true
-  validates :content, presence: true
-  validates :content, presence: true
-  validates :content, presence: true
-  validates :content, presence: true
-  validates :content, presence: true
+  validates :item_name, presence: true
+  validates :item_detail, presence: true
+  validates :item_condition_id, presence: true
+  validates :item_category_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :days_to_ship_id, presence: true
+  
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category,:condition,:day_to_ship,:delivery_charge,:prefecture
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true,format: { with: /\A[0-9]+\z/ } do
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  presence:{message:"can't be blank"}
+  end
 end
