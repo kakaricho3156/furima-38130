b28f18e7d5dfc59610101198cffc3433f934233a
kakaricho3156 <kakaricho1216@gmail.com>
2022-07-19 16:20:50 +0900

    テスト完了

---
+++ spec/factories/buying_item_form.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :buying_item_form do
+   user_id{Faker::Number.non_zero_digit}
+   item_id{Faker::Number.non_zero_digit}
+   post_number{Faker::Number.decimal_part(digits:3) + "-" +Faker::Number.decimal_part(digits:4)}
+   prefecture{Faker::Number.between(from:1,to:47)}
+   town{Faker::Address.city}
+   address{Faker::Address.street_address}
+   building{Faker::Address.street_address}
+   telephone{Faker::Number.decimal_part(digits:11)}
+   token { Faker::Internet.password(min_length: 20, max_length: 30) }
+  end
+end
+
+ 
